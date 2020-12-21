9cb5cd1c1590c1fcd7a77b3b588f4664d7231026
rthngl <56600718+rthngl@users.noreply.github.com>
2020-12-21 12:50:04 -0800

    glitch

--- celebs/app.js
+++ celebs/app.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 const navLogo = document.querySelector('#navbar__logo')
-const mainbutton = document.querySelector('#main__btn')
 
 // display mobile menu, this is a function
 const mobileMenu = () => {
@@ -58,57 +57,57 @@ navLogo.addEventListener('click', hideMobileMenu);
 //firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyAe2e1wi_qjzlD4HR6ZMeYutKDveB-K-wM",
-    authDomain: "celebs--rae.firebaseapp.com",
-    databaseURL: "https://celebs--rae-default-rtdb.firebaseio.com",
-    projectId: "celebs--rae",
-    storageBucket: "celebs--rae.appspot.com",
-    messagingSenderId: "427947460588",
-    appId: "1:427947460588:web:f471bfdc2c6d5bc5d722c9",
-    measurementId: "G-3YTV00EREN"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAe2e1wi_qjzlD4HR6ZMeYutKDveB-K-wM",
+//     authDomain: "celebs--rae.firebaseapp.com",
+//     databaseURL: "https://celebs--rae-default-rtdb.firebaseio.com",
+//     projectId: "celebs--rae",
+//     storageBucket: "celebs--rae.appspot.com",
+//     messagingSenderId: "427947460588",
+//     appId: "1:427947460588:web:f471bfdc2c6d5bc5d722c9",
+//     measurementId: "G-3YTV00EREN"
+//     };
+//     // Initialize Firebase
+//     firebase.initializeApp(firebaseConfig);
+//     firebase.analytics();
 
-//trying this variable declaration
-import firebase from "firebase/app";
-import "firebase/database";
-var database = firebase.database();
-var myFirebase = firebase.database().ref();
-var inBowl = myFirebase.child("inBowl");
+// //trying this variable declaration
+// import firebase from "firebase/app";
+// import "firebase/database";
+// var database = firebase.database();
+// var myFirebase = firebase.database().ref();
+// var inBowl = myFirebase.child("inBowl");
 
-// saving info?
-const submitname = () => {
-    inBowl.update({userinput})
-    //document.write("yo" + userinput);
-};
-mainbutton.addEventListener('click', submitname);
+// // saving info?
+// const submitname = () => {
+//     inBowl.update({userinput})
+//     //document.write("yo" + userinput);
+// };
+// mainbutton.addEventListener('click', submitname);
 
-//tutorial
-var config = {
-    apiKey: "AIzaSyAe2e1wi_qjzlD4HR6ZMeYutKDveB-K-wM",
-    authDomain: "celebs--rae.firebaseapp.com",
-    databaseURL: "https://celebs--rae-default-rtdb.firebaseio.com",
-    projectId: "celebs--rae",
-    storageBucket: "celebs--rae.appspot.com",
-    messagingSenderId: "427947460588",
-    appId: "1:427947460588:web:f471bfdc2c6d5bc5d722c9",
-    measurementId: "G-3YTV00EREN"
-  };
+// //tutorial
+// var config = {
+//     apiKey: "AIzaSyAe2e1wi_qjzlD4HR6ZMeYutKDveB-K-wM",
+//     authDomain: "celebs--rae.firebaseapp.com",
+//     databaseURL: "https://celebs--rae-default-rtdb.firebaseio.com",
+//     projectId: "celebs--rae",
+//     storageBucket: "celebs--rae.appspot.com",
+//     messagingSenderId: "427947460588",
+//     appId: "1:427947460588:web:f471bfdc2c6d5bc5d722c9",
+//     measurementId: "G-3YTV00EREN"
+//   };
   
-  // Initialize your Firebase app
-  firebase.initializeApp(config);
+//   // Initialize your Firebase app
+//   firebase.initializeApp(config);
   
-  // Reference to your entire Firebase database
-  var myFirebase = firebase.database().ref();
+//   // Reference to your entire Firebase database
+//   var myFirebase = firebase.database().ref();
   
-  // Get a reference to the recommendations object of your Firebase.
-  // Note: this doesn't exist yet. But when we write to our Firebase using
-  // this reference, it will create this object for us!
-  var recs = myFirebase.child("inBowl");
+//   // Get a reference to the recommendations object of your Firebase.
+//   // Note: this doesn't exist yet. But when we write to our Firebase using
+//   // this reference, it will create this object for us!
+//   var recs = myFirebase.child("inBowl");
   
-  // Push our first recommendation to the end of the list and assign it a
-  // unique ID automatically.
-  recs.push("try this");
+//   // Push our first recommendation to the end of the list and assign it a
+//   // unique ID automatically.
+//   recs.push("try this");
